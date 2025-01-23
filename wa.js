8769e5b713365f1cf204bb55afa747bc29326c1d
khadijaDev <khadijaoukhlift@gmail.com>
2025-01-23 16:53:05 +0100

    Starting the javascript part

---
+++ wa.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", async () => {
+  const res = await "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}&lang={lang}";
+   const 
+})
+
+
